d33bad16007d0a21afb060844994a7ac54967f71
StationedInTheField <ask@stationedinthefield.com>
2020-09-08 04:41:01 -0700

    os refactor

--- src/app/services/quest-os.service.ts
+++ src/app/services/quest-os.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { qOS }  from '@questnetwork/quest-os-js';
 import * as swarmJson from '../swarm.json';
-
+import  packageJson from '../../../package.json';
+import { ElectronService } from 'ngx-electron';
+const version = packageJson.version;
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +13,14 @@ export class QuestOSService {
   public os;
   ready = false;
   config;
-  constructor() {
+  constructor(private electron: ElectronService) {
     this.config = {
       ipfs: {
         swarm: swarmJson['ipfs']['swarm']
+      },
+      version: version,
+      dependencies: {
+        electronService: this.electron
       }
     };
     this.os = qOS;
