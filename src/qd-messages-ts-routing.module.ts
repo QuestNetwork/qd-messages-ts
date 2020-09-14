5e8050b9c0657c19e2ea2c3e36ef3644fb48e5bc
StationedInTheField <ask@stationedinthefield.com>
2020-09-14 01:37:57 -0700

    qdesk refac

--- src/qd-messages-ts-routing.module.ts
+++ src/qd-messages-ts-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { QuestMessengerJSComponent } from './quest-messenger-js.component';
+import { QuestMessengerJSComponent } from './qd-messages-ts.component';
 
 const routes: Routes = [
   { path: '', component: QuestMessengerJSComponent }
